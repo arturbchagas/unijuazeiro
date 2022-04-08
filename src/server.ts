458fc825ce877377a82935466d881088de3798f8
arturbchagas <arturbbcc@gmail.com>
2022-04-08 17:06:54 -0300

    atv

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,46 @@
 const prompt = require("prompt-sync")();
-let x = prompt("digita ae ");
 
 
-if (x<0) {
-    console.log("numero negativo");
-}else{
-    console.log("numero positivo");
+function soma(){
+
+    
+    let x = 0
+    let y = 0
+    let soma = 0
+    
+    do {
+    x =parseInt(prompt("Escreva o primeiro numero"));
+    y =parseInt(prompt("Escreva o segundo numero"));
+        
+    } while (x < 0 && y < 0);
+    
+
+    for (let i = x; i < y; i++) {
+        soma = soma + i   
+    }
+    
+    console.log(soma)
+
 }
+
+soma()
+
+
+
+ class Pessoa{
+     nome:string;
+     idade:number;
+    constructor( nome:string,idade:number){
+     this.nome = nome;
+     this.idade = idade; 
+    
+     }
+
+     calcularIdade(){
+         return this.idade * 12;
+     }
+}
+
+ const informacoes = new Pessoa('Artur',24);
+
+ console.log(informacoes.calcularIdade());
